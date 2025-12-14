fb4182b923df31a5452bfa01a7a3b9916e325c0c
carlos-arino <arino@uji.es>
2025-12-14 23:18:45 +0100

    Actualizar el archivo main.cpp para el ejercicio entregable 101 y modificar el tipo de componente en diagram.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-/* Ejercicio entregable 100
- Deben de utilizar este archivo los alumnos con c<5, d<5, u>=5
- siendo c,d,u las tres últimas cifras del DNI 22000cdu -W
-Temperatura y motor paso a paso con encoder
- Para cambiar la Temperatura o haz click sobre el sensor DS18B20 durante la simulacion
+/* Ejercicio entregable 101
+ Deben de utilizar este archivo los alumnos con c>=5, d<5, u>=5
+ siendo c, d y u las tres últimas cifras del DNI 22000cdu -W
+ Sensor de temperatura y motor paso a paso con potenciometro
+ Para cambiar la temperatura haz click sobre el anemometro durante la simulacion
  rellenar vuestro nombre y DNI
  NOMBRE ALUMNO: XXXXX
  DNI: XXXXX
- ENLACE WOKWI: XXXXXXX
+ ENLACE WOKWI: XXXXXXXXXXX
 */
+
 #include <Stepper.h>
 #include <Arduino.h>
 #include <OneWire.h>
@@ -15,11 +16,8 @@ Temperatura y motor paso a paso con encoder
 
 Stepper myStepper(200, 14, 27, 26, 25);
 
-const int encAPin = 23; // Pin para el canal A del encoder
-const int encBPin = 22; // Pin para el canal B del encoder
-const int encCPin = 16; // Pin para el canal C del encoder
-const int encDPin = 15; // Pin para el canal D del encoder
-const int tempPin = 4;  // Pin de datos del DS18B20 (OneWire)
+const int potPin = 34; // Pin del potenciometro
+const int tempPin = 4; // Pin de datos del DS18B20 (OneWire)
 
 OneWire oneWire(tempPin);
 DallasTemperature sensors(&oneWire);
@@ -30,10 +28,6 @@ void setup()
   Serial.begin(115200);
   Serial.println("Hello, ESP32!");
   myStepper.setSpeed(10);
-  pinMode(encAPin, INPUT);
-  pinMode(encBPin, INPUT);
-  pinMode(encCPin, INPUT);
-  pinMode(encDPin, INPUT);
   sensors.begin();
 }
 
@@ -49,13 +43,7 @@ void loop()
     sensors.requestTemperatures();
     const float tempC = sensors.getTempCByIndex(0);
 
-    Serial.print(digitalRead(encAPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encBPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encCPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encDPin));
+    Serial.print(analogRead(potPin));
     Serial.print(", ");
     Serial.println(tempC);
   }
@@ -69,13 +57,7 @@ void loop()
     sensors.requestTemperatures();
     const float tempC = sensors.getTempCByIndex(0);
 
-    Serial.print(digitalRead(encAPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encBPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encCPin));
-    Serial.print(", ");
-    Serial.print(digitalRead(encDPin));
+    Serial.print(analogRead(potPin));
     Serial.print(", ");
     Serial.println(tempC);
   }
